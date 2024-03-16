ef80db719f20a5899d7c53251dd212d845b6fe9d
Believeharsh <bontinue@gmail.com>
2024-03-16 23:14:10 +0530

    formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Login from "./Components/Login-Signup/Login"
 import Navbar from "./Components/Navbar/Navbar"
-// import Register from "./Components/Register/Register"
 
 import Sidebar from "./Components/Sidebar/Sidebar"
 
