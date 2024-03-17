714c3d9e0b1e2a1dd7159e900687e0db9bca288e
Believeharsh <bontinue@gmail.com>
2024-03-17 15:51:01 +0530

    Integrated Flowbite into the react application

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import Login from "./Components/Login-Signup/Login"
-import Navbar from "./Components/Navbar/Navbar"
-// import Register from "./Components/Register/Register"
-
-import Sidebar from "./Components/Sidebar/Sidebar"
+import Login from "./Components/Login-Signup/Login";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
- 
-
   return (
     <>
-      
-      <Navbar/>
-      {/* <Sidebar/> */}
-      <Login/>
-    </>
-  )
+      <Navbar />
+      <Sidebar/>
+      {/* <Login /> */}
+  
+    </> 
+  );
 }
 
-export default App
+export default App;
