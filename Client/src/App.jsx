0962961f9541262ddf55273817e97eccc7affa34
Believeharsh <bontinue@gmail.com>
2024-03-22 21:56:34 +0530

    moved all files to Client folder

---
+++ Client/src/App.jsx
@@ -0,0 +1,25 @@
+
+import ImageSlider from "./Components/ImageSlider/ImageSlider";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <Sidebar/> */}
+      <ImageSlider/>
+      
+   
+
+      
+  
+    </> 
+  );
+}
+
+export default App;
